5128698380669455cb81995f00bbe206656ee22f
neudabei <rschultzejena@gmail.com>
2015-12-03 12:33:40 +0000

    add scenarios for invalid or no URLs to test suite

--- spec/controllers/urls_controller_spec.rb
+++ spec/controllers/urls_controller_spec.rb
@@ -2,25 +2,37 @@ require 'rails_helper'
 
 describe UrlsController do
   describe "POST Create" do
-    before do
-      post :create, url: "http://www.sueddeutsche-zeitung.de/"
+    context "correct URL entered" do
+      before do
+        post :create, url: "http://www.sueddeutsche-zeitung.de/"
+      end
+
+      it "generates a short string" do
+        expect(session[:urls].keys.first.length).to eq(6)
+      end
+
+      it "stores a new short url in the session hash" do
+        expect(session[:urls]).to eq({session[:urls].keys.first => "http://www.sueddeutsche-zeitung.de/"})
+      end
+
+      it "stores multiple urls in the session hash" do
+        post :create, url: "http://www.google.com/"
+        expect(session[:urls].keys.count).to eq(2)
+      end
+
+      it "renders the new template" do
+        expect(response).to redirect_to root_path
+      end
     end
 
-    it "generates a short string" do
-      expect(session[:urls].keys.first.length).to eq(6)
+    context "incorrect URL entered" do
+      it "sets flash error message"
+      it "it doesn't write to sessions hash"
     end
 
-    it "stores a new short url in the session hash" do
-      expect(session[:urls]).to eq({session[:urls].keys.first => "http://www.sueddeutsche-zeitung.de/"})
-    end
-
-    it "stores multiple urls in the session hash" do
-      post :create, url: "http://www.google.com/"
-      expect(session[:urls].keys.count).to eq(2)
-    end
-
-    it "renders the new template" do
-      expect(response).to redirect_to root_path
+    context "no URL entered" do
+      it "sets flash error message"
+      it "doesn't write to sessions hash"
     end
   end
 
